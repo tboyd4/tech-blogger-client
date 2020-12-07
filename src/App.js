9fcc64fc1c0b09779b429acc1a09ccc52663b057
TylerBoyd <tboyd1898@gmail.com>
2020-12-07 11:13:24 -0600

    added stylse to artcle cards

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 // css imports
-import './css/App.css';
+import "./css/App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 // package imports
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 // component imports
-import Navbar from './components/global-components/navbar.component';
-import PostList from './components/page-components/post-list.component';
-import AddPost from './components/page-components/add-post.component';
-import EditPost from './components/page-components/edit-post.component';
-import AddAuthor from './components/page-components/add-author.component';
+import Navbar from "./components/global-components/navbar.component";
+import PostList from "./components/page-components/post-list.component";
+import AddPost from "./components/page-components/add-post.component";
+import EditPost from "./components/page-components/edit-post.component";
+import AddAuthor from "./components/page-components/add-author.component";
 
 function App() {
   return (
     <Router>
       <Navbar></Navbar>
       <br></br>
-      <Route path="/" exact component={PostList} />
-      <Route path="/edit/:id" component={EditPost} />
-      <Route path="/add/post" component={AddPost} />
-      <Route path="/add/author" component={AddAuthor} />
+      <div className="container">
+        <Route path="/" exact component={PostList} />
+        <Route path="/edit/:id" component={EditPost} />
+        <Route path="/add/post" component={AddPost} />
+        <Route path="/add/author" component={AddAuthor} />
+      </div>
     </Router>
   );
 }
--- src/components/page-components/post-list.component.js
+++ src/components/page-components/post-list.component.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 
 const Post = (props) => (
-  <div className="card">
+  <div className="card my-2 py-1 shadow">
     <div className="card-body">
       <h5 className="card-title">{props.post.title}</h5>
       <h6 className="card-subtitle mb-2 text-muted">
